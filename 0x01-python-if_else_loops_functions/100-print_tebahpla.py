f59695938618328a650930f8c07b221963dd5edf
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-09-06 04:47:50 +0300

    ???

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{}".format(chr(i) if i % 2 == 0 else chr(i - 32)), end="")
+for i in range(25, -1, -1):
+    c = i + ord('A')
+    if i % 2 == 1:
+        c += 32
+    print("{:c}".format(c), end="")
