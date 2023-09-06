2df18eaaed8e4897628e73a7f8064c896f5eb56c
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-09-06 04:35:55 +0300

    no vars

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
-    c = i
-    if i % 2 != 0:
-        c = i - (ord('a') - ord('A'))
-    print("{}".format(chr(c)), end="")
+    print("{:c}".format(i if i % 2 == 0 else i - (ord('a') - ord('A'))), end="")
