53d0c672edfb9b4f945dc2993d7463fe2c95e1ed
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-09-06 03:18:03 +0300

    6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(8):
-	for j in range(i+1, 10):
-		print(f"{i}", end="")
-		print(f"{j}", end=", ")
+    for j in range(i+1, 10):
+        print(f"{i}", end="")
+        print(f"{j}", end=", ")
 print(89)
