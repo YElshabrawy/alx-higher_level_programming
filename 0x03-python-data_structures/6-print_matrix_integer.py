58d9fa01d89ee2b6cc18beb4aa62502313b5dae8
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-09-10 17:13:17 +0300

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        if (len(row) == 0):
+            print()
+        for idx, num in enumerate(row):
+            print("{:d}".format(num), end="\n" if idx == len(row) - 1 else " ")
