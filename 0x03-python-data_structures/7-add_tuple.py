4526f90aeccf75560e370970e1e1e038ff4e8640
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-09-10 17:21:22 +0300

    tuple add

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    sum0 = 0
+    sum1 = 0
+
+    sum0 += tuple_a[0] if len(tuple_a) > 0 else 0
+    sum0 += tuple_b[0] if len(tuple_b) > 0 else 0
+
+    sum1 += tuple_a[1] if len(tuple_a) > 1 else 0
+    sum1 += tuple_b[1] if len(tuple_b) > 1 else 0
+    return sum0, sum1
