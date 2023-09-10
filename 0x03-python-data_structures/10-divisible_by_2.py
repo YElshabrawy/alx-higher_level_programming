871996b65b6a56d7b02d89907a881108cd73bdff
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-09-10 17:49:58 +0300

    div by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if not my_list or len(my_list) == 0:
+        return None
+    out = []
+    for num in my_list:
+        if num % 2 == 0:
+            out.append(True)
+        else:
+            out.append(False)
+    return out
