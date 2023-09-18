0bb7062b48b33bf474166d6eecc43f1e60b783f2
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-09-18 10:49:50 +0300

    magic

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    pass
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(4, 6)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
+    return 0
