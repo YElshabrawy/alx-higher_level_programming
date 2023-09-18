d6fae9f703c2e3799aeaa8cd81d7a12b8a9d945b
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-09-18 09:53:31 +0300

    imported

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
 
-a, b = 1, 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == '__main__':
+    a, b = 1, 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
