79aa1866b24a543d86e57bd584ef7b6162d93b80
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-09-27 04:50:55 +0300

    4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            size: size of square
+        """
+        size(size)
+
+    def area(self):
+        """Returns area of square"""
+        return self.__size * self.__size
+
+    def size(self):
+        return self.__size
+
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
