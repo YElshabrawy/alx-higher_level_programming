7d4d8b262a1e8ffe4941803593e27bcccfc2dd5e
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-09-27 04:37:43 +0300

    sq

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """Constructor.
+
+        Args:
+            size: size of square
+        """
+        self.__size = size
