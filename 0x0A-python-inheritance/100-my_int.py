1450750aa85b8534a072a8a53505df2daef27a97
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-11-17 18:45:21 +0200

    ..

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,5 +6,6 @@ class MyInt(int):
     """mylist class"""
     def __eq__(self, __value: object) -> bool:
         return super().__ne__(__value)
+
     def __ne__(self, __value: object) -> bool:
         return super().__eq__(__value)
