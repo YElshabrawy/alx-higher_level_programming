81775f7bc62669072521b19d0f8ad3b93c843be7
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-11-17 13:43:02 +0200

    ..

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -67,3 +67,9 @@ class Rectangle(Base):
     def area(self):
         """returns area"""
         return self.width * self.height
+
+    def display(self):
+        """prints rect #"""
+        dispChar = '#'
+        for _ in range(self.height):
+            print(dispChar * self.width)
