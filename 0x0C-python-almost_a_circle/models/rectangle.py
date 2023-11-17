ac7bc7378cb7cd63b9a1e6fa640d70dce9e92eca
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-11-17 13:47:13 +0200

    ..

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,4 +75,5 @@ class Rectangle(Base):
             print(dispChar * self.width)
 
     def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+        return "[Rectangle] ({}) {}/{} - {}/{}" \
+            .format(self.id, self.x, self.y, self.width, self.height)
