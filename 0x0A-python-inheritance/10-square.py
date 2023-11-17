2fc69dc3fcab2d2024536cc2bb3718c055a867a0
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-11-17 18:36:39 +0200

    ..

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""mylist module"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """mylist class"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
