a1e32c5d48d6c215a38cbd5a8a2481bb84e0af1b
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-11-17 18:43:58 +0200

    ..

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""mylist module"""
+
+
+class MyInt(int):
+    """mylist class"""
+    def __eq__(self, __value: object) -> bool:
+        return super().__ne__(__value)
+    def __ne__(self, __value: object) -> bool:
+        return super().__eq__(__value)
