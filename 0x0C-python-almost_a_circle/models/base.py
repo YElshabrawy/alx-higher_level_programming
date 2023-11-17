577d2d089411095f8cca86b18a656b553a78ff94
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-11-17 13:14:46 +0200

    ..

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ base class """
+
+
 class Base:
     """ Base class """
     __nb_objects = 0
+
     def __init__(self, id=None):
         """Constructor"""
         if id is not None:
