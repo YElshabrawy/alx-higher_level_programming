428245448c8c10857b33bd4885c9fcf54b382bb3
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-11-17 14:01:37 +0200

    ..

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,3 +78,16 @@ class Rectangle(Base):
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}" \
             .format(self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args):
+        """update vals (id, w, h, x, y)"""
+        if len(args) > 0 and args[0] is not None:
+            self.id = args[0]
+        if len(args) > 1 and args[1] is not None:
+            self.width = args[1]
+        if len(args) > 2 and args[2] is not None:
+            self.height = args[2]
+        if len(args) > 3 and args[3] is not None:
+            self.x = args[3]
+        if len(args) > 4 and args[4] is not None:
+            self.y = args[4]
