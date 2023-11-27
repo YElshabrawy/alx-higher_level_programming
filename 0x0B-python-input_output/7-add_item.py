cee045047f905528d694d0d5a732674e2e279c55
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-11-27 18:16:31 +0200

    ..

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Module'''
+from sys import argv
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+argv.pop(0)
+
+try:
+	oldItms = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    oldItms = []
+
+for itm in argv:
+    oldItms.append(itm)
+save_to_json_file(oldItms, 'add_item.json')
