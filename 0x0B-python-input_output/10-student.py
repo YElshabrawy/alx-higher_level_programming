c436fc20046c6acb3bf9c1e3c7bfc517815bd116
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-11-27 18:29:43 +0200

    ..

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Module'''
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        '''init'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''return json'''
+        attr_dict = self.__dict__
+        if type(attrs) is None:
+            return attr_dict
+
+        out = {}
+        for key in attrs:
+            val = attr_dict.get(key)
+            if val is not None:
+                out[key] = val
+
+        return out
