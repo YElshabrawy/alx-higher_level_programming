4c41438d7c75770423f9a1dbb45d78a5420a2aa3
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-12-15 02:10:47 +0200

    ..

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 --
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_show_genres
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
 HAVING number_of_shows > 0
