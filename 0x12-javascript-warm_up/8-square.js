e5a0cc8b648bddcda5566454dbd30e5a78cf011c
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-11 00:49:05 +0200

    ..

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const n = parseInt(process.argv[2]);
+if (!n) console.log('Missing size');
+else {
+  for (let i = 0; i < n; i++) {
+    let line = '';
+    for (let j = 0; j < n; j++) line += 'X';
+    console.log(line);
+  }
+}
