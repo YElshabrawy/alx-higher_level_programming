91df07b681f394e908f1a512916a10a49b7d307d
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-11 01:11:38 +0200

    ..

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,6 @@
 let arr = process.argv;
 arr.shift();
 arr.shift();
-arr = arr.map((itm) => parseInt(itm));
-arr.sort().reverse();
+arr = arr.map((itm) => parseInt(itm)).filter((val) => !Number.isNaN(val));
+arr = arr.sort((a, b) => b - a);
 console.log(arr[1] || 0);
