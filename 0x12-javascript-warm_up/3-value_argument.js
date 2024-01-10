c6c6efdcdc95272a187669842eaa89247cef8780
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-11 00:23:03 +0200

    ..

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,7 +3,7 @@ let c = 0;
 process.argv.shift();
 process.argv.shift();
 process.argv.forEach((arg) => {
-    console.log(arg);
-    c++;
+  console.log(arg);
+  c++;
 });
 if (c === 0) console.log('No argument');
