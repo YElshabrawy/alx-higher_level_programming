8ea502bc00a09c318528b8aa08a9d59adc6e2921
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-11 13:51:06 +0200

    ..

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,30 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w && h && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) { line += 'X'; }
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
