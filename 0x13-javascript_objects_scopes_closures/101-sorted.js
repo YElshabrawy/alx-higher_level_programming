e2fac34091a982da24300c83621c6fe065cf7ba3
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-11 14:35:11 +0200

    ..

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const dict = require('./101-data').dict;
+const output = {};
+Object.entries(dict).forEach(([key, val]) => {
+  const idx = Object.keys(output).findIndex(k => k.toString() === val.toString());
+  if (idx === -1) {
+    // If val not in output, create it
+    output[val] = [key];
+  } else {
+    // If val exists, append into its arr
+    output[val] = [...output[val], key];
+  }
+});
+console.log(output);
