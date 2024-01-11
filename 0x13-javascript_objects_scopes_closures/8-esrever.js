9420c9beea082b36ba14b7d5b2efc3bacb9997e4
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-11 14:05:45 +0200

    ..

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return list.reverse();
+  const output = [];
+  for (let i = list.length - 1; i >= 0; i--) { output.push(list[i]); }
+  return output;
 };
