8df6c0b6109d4372ce356a783a23d6c67a00bbd5
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-11 13:59:28 +0200

    ..

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const _Square = require('./5-square');
+
+class Square extends _Square {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) { line += c; }
+      console.log(line);
+    }
+  }
+}
+
+module.exports = Square;
