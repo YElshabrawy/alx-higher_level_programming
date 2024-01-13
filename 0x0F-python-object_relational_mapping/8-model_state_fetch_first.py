f84811a6445a27864f9551ba0c308b85a0334466
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-13 21:38:49 +0200

    ..

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+'''module'''
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    dbUser, dbPass, dbName = argv[1], argv[2], argv[3]
+
+    dbURL = "mysql+mysqldb://{}:{}@localhost:3306/{}" \
+        .format(dbUser, dbPass, dbName)
+    engine = create_engine(dbURL, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    state = s.query(State).first()
+    if state is not None:
+        print(f"{state.id}: {state.name}")
+    else:
+        print("Nothing")
+    s.commit()
+    s.close()
