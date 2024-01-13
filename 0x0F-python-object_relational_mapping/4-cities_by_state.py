7c199359e91c3f315044c9e79e252a9e02584dfb
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-13 20:38:22 +0200

    ..

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+'''Lists all states'''
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    dbUser = argv[1]
+    dbPass = argv[2]
+    dbName = argv[3]
+    db = MySQLdb.connect(password=dbPass, database=dbName,
+                         user=dbUser, host='localhost', port=3306)
+    c = db.cursor()
+    c.execute('''SELECT cities.id, cities.name, states.name
+              FROM states
+              LEFT JOIN cities ON cities.state_id = states.id
+              ORDER BY id''')
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
