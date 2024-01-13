0f14438d9acde6fed266a1bda91d2e51c1234300
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-13 20:09:48 +0200

    ..

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+'''Lists all states'''
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    dbUser = argv[1]
+    dbPass = argv[2]
+    dbName = argv[3]
+    stateName = argv[4]
+    db = MySQLdb.connect(password=dbPass, database=dbName,
+                         user=dbUser, host='localhost', port=3306)
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name = \"{}\" ORDER BY id"
+              .format(stateName.strip("'")))
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
