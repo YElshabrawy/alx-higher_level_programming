46d7d1bd7c1dbd200022e94a1e4e5a82110439b3
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-13 22:23:35 +0200

    ..

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""module"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    dbUser, dbPass, dbName = argv[1], argv[2], argv[3]
+
+    dbURL = "mysql+mysqldb://{}:{}@localhost:3306/{}" \
+        .format(dbUser, dbPass, dbName)
+    engine = create_engine(dbURL)
+    Session = sessionmaker(engine)
+    s = Session()
+    s.query(State).filter(State.name.contains('a')).delete()
+    s.commit()
+    s.close()
