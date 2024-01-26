00bc08d7350dce81c1c03d5cf18f6849d0cf83c1
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-26 02:35:34 +0200

    ..

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+'''finds peak of a list'''
+
+def find_peak(list_of_integers):
+    """finds max in an unsorted list of integers"""
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    for i in range(len(list_of_integers)):
+        try:
+            if list_of_integers[i - 1] <= list_of_integers[i]:
+                if list_of_integers[i + 1] <= list_of_integers[i]:
+                    return list_of_integers[i]
+        except IndexError:
+            try:
+                if list_of_integers[i] >= list_of_integers[i+1]:
+                    return list_of_integers[i]
+            except IndexError:
+                if list_of_integers[i] > list_of_integers[i - 1]:
+                    return list_of_integers[i]
+    return None
