9c10a8a4c0b6f07c4203c55c2e0ef4bdcc429c98
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-26 18:39:59 +0200

    ..

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+'''m'''
+import urllib.request as ur
+from sys import argv
+import urllib.parse as up
+
+if __name__ == "__main__":
+    data = up.urlencode({'email': argv[2]}).encode('utf-8')
+    req = ur.Request(argv[1], data)
+    with ur.urlopen(req) as res:
+        print(res.read().decode('utf-8'))
