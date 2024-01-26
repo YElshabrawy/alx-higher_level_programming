38dd566224bce35cc7372da950f6cb9f228100ed
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-26 23:32:54 +0200

    ..

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+'''v'''
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    REPO = argv[1]
+    OWNER = argv[2]
+    url = f"https://api.github.com/repos/{OWNER}/{REPO}/commits"
+    res = requests.get(url, params={"per_page": 10})
+    commits = res.json()
+    for c in commits:
+        print(f"{c['sha']}: {c['commit']['author']['name']}")
