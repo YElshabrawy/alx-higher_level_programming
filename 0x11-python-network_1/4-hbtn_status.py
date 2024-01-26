b8c7223700016211d7251ce9110a6caba234cb56
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-26 18:50:45 +0200

    ..

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ import requests
 if __name__ == "__main__":
     res = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print(f"\t- type: {type(res.text)}")
-    print(f"\t- content: {res.text}")
+    print("\t- type:", type(res.text))
+    print("\t- content:", res.text)
