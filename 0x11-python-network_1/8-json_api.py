c2ff1cce968dcae98c7bf195137bcb364ab622fa
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-26 19:37:59 +0200

    ..

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+'''g'''
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "http://63f92135de34.92814df4.alx-cod.online:5000/search_user"
+    if len(argv) == 1:
+        q = ""
+    else:
+        q = argv[1]
+    try:
+        res = requests.post(url, {"q": q}).json()
+        if res:
+            print(f"[{res['id']}] {res['name']}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
