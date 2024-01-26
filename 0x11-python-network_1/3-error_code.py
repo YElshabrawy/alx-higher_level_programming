937e2423b4d51df9f7cbd05abc8743832a628fee
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-26 18:45:30 +0200

    ..

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+''','''
+import urllib.request
+import urllib.error
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as res:
+            print(res.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
