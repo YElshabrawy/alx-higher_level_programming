e9c14ba833cddcbb7ba58300138c46f586411e24
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-26 23:22:26 +0200

    ..

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+'''g'''
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = f"https://api.github.com/user"
+    res = requests.get(url, auth=(argv[1], argv[2]))
+    if res.status_code == 200:
+        print(res.json()['id'])
+    else:
+        print(None)
