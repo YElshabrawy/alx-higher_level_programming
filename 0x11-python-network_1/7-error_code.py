d3fb2ec5cc5881ff394198df2408bd6bf8386fa2
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-26 19:05:42 +0200

    ..

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+'''v'''
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    res = requests.get(argv[1])
+    code = res.status_code
+    if code >= 400:
+        print("Error code:", code)
+        exit(1)
+    print(res.text)
