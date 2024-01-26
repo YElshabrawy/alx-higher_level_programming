c43d410742846d385754d487caded8a3ffcfeee5
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-26 18:56:17 +0200

    ..

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+'''g'''
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    res = requests.get(argv[1])
+    id = res.headers.get("X-Request-Id")
+    print(id)
