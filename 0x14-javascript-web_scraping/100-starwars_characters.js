fc7d72e17531c8873a10eba4b6203e9b9ef091d1
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-04-23 23:34:00 +0200

    100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+
+request.get(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const characters = JSON.parse(body).characters;
+    const promises = characters.map(character => new Promise((resolve, reject) => {
+      request.get(character, (err, res, body) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(body).name);
+        }
+      });
+    }));
+    Promise.all(promises).then(characters => {
+      characters.forEach(character => console.log(character));
+    }).catch(err => console.log(err));
+  }
+}
+);
