3cdf1b8bf4372313df7cb82693bb2d6aaa83d2dc
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-04-23 23:11:36 +0200

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const results = JSON.parse(res.body).results.filter((movie) => {
+      return movie.characters.some((el) => el.endsWith('18/'));
+    });
+    console.log(results.length);
+  }
+});
