e48ef4751f2232ad15395fed20bdc109b56f95c0
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-04-23 23:16:49 +0200

    5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const fs = require('fs')
+const request = require('request')
+const url = process.argv[2];
+const filename = process.argv[3];
+
+request(url, (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(filename, res.body, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    })
+  }
+})
