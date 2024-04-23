fa9b7ea534b8971ac44ffd95c3c8f466968c1bca
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-04-23 23:22:32 +0200

    6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, res) => {
+  const output = {};
+  const data = JSON.parse(res.body);
+  data.forEach(el => {
+    if (!output[el.userId]) {
+      output[el.userId] = 0;
+    }
+    if (el.completed) {
+      output[el.userId]++;
+    }
+  });
+  console.log(output);
+});
