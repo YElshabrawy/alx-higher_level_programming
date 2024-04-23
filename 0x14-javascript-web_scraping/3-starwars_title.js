f83e602b017fc1a8242deed5ecfe978c29339f16
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-04-23 22:59:56 +0200

    3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(res.body).title);
+  }
+});
